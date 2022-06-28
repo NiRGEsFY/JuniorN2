8c9e615d26f22bfcc1421d1836b890427067ff8a
Nick Rudakov <porol2122@gmail.com>
2022-06-28 23:51:03 +0300

    Junior

---
+++ Solution for Test # 2.cs
@@ -0,0 +1,27 @@
+Solution for Test # 2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IJuniorForGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isTestingProcess = false;
+            int number = -3; 
+            string text = "Any texts";
+            double bigNumber = 6.15252;
+            float notIsBigNumber = 5512;
+            char symbol = 'G';
+            long soHugeNumber = 5125346245624356134;
+            uint noNegativeNumber = 5;
+            decimal soBigNumber = 7112351235622363467;
+            byte smallNumber = 4;
+            short normalNumber = 12345;
+        }
+    }
+}
